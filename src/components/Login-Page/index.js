8c3349aa8881c45f90c8338e1bd8b83877cb9000
ramadika <rzdka18@gmail.com>
2021-03-03 09:49:03 +0700

    login access

--- src/components/Login-Page/index.js
+++ src/components/Login-Page/index.js
@@ -4,8 +4,9 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import { Redirect } from 'react-router-dom'
+import Axios from 'axios';
 import ImgBG from 'assets/thomas-jensen-h3vT1Kp0FxA-unsplash.jpg'
-import { PostData } from 'components/Login-Page/Post-Data'
+// import { PostData } from 'components/Login-Page/Post-Data'
 // Internals
 import 'components/Login-Page/index.css'
 import {DataContext} from 'components/Context'
@@ -15,50 +16,41 @@ export default class Index extends Component{
     constructor(){
         super();
         this.state = {
-            Username: '',
-            Password: '',
             redirectToReferrer: false,
             count: 0
         };
-        this.login = this.login.bind(this);
-        this.onChange = this.onChange.bind(this);
     }
 
-    login() {
-        if(this.state.Username && this.state.Password){ 
-            PostData(this.state).then((result) => {
-                let responseJson = result;
-                if(responseJson.user){ 
-                    // sessionStorage.setItem('user',JSON.stringify(responseJson));
-                    this.setState({
-                        redirectToReferrer: true,
-                        count: 1
-                    });
-                }else {
-                    alert(responseJson.msg);
-                }
-            });
-        }else {
-            PostData(this.state).then((result) => {
-                alert(result.msg);
-            });
-        }
-    }
-
-    onChange(e){
-        this.setState({
-            [e.target.name]:e.target.value
+    insertUser = (event) => {
+        event.preventDefault();
+        event.persist();
+        Axios.post('http://localhost/backend-app/login.php',{
+            Username: this.Username.value,
+            Password: this.Password.value
+        })
+        .then(function ({data}) {
+            if(data.success === 1){;
+                this.setState({
+                    redirectToReferrer: true,
+                    count: 1
+                });
+                alert(data.msg)
+            }
+            else{
+                alert(data.msg)
+            }
+        }.bind(this))
+        .catch(function (error) {
+            console.log(error);
         });
     }
 
     render(){
 
         if (this.state.redirectToReferrer ){
-            // if (sessionStorage.getItem('user')){
-                return (
-                    <Redirect to={'/HomePage'}/>
-                )
-            // }
+            return (
+                <Redirect to={'/HomePage'}/>
+            )
         } 
 
         return(
@@ -73,16 +65,19 @@ export default class Index extends Component{
                         </Col>
                         <Col className="lgnCol text-left">
                             <h2 className="mb-3">Login</h2>
-                            {/* <form> */}
+                            <form  onSubmit={this.insertUser} className="form-account">
                                 <div className="inputLogin-custom">
+
                                     <h6 className="usPsLog">Username</h6>
-                                    <input type="text" name="Username" onChange={this.onChange} className="form-control" placeholder="Username" />
+                                    <input type="text" name="Username" ref={(val) => this.Username = val} className="form-control" placeholder="Username" />
+
                                     <h6 className="usPsLog">Password</h6>
-                                    <input type="password" name="Password" onChange={this.onChange} className="form-control" placeholder="Password"/>
-                                    <input className=" btnLogin-custom mb-5" type="submit" value="Sign in" onClick={this.login}></input>
-                                    {/* <button type="submit" class="btnLogin-custom mb-5" onClick={this.login}>Sign in</button> */}
+                                    <input type="password" name="Password" ref={(val) => this.Password = val} className="form-control" placeholder="Password"/>
+
+                                    <input type="submit" value="Sign in"  className=" btnLogin-custom mb-5"></input>
+                                    
                                 </div>
-                            {/* </form> */}
+                            </form>
                         </Col>
                     </Row>
                 </Container>
